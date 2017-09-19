a9554cd36f38904a82b574e276f5e31fe4c50049
Rafi <soliniak@gmail.com>
2017-09-20 01:02:15 +0200

    MoGo WebDev Challange #35

---
+++ Gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require('gulp'),
+	sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minify = require('gulp-minifier'),
+	babel = require("gulp-babel");
+
+
+// Static server
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+});
+
+
+gulp.task('prefix', function(){
+
+    gulp.src('css/**/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('mini', function() {
+  return gulp.src('**/*').pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyJS: true,
+    minifyCSS: true,
+    getKeptComment: function (content, filePath) {
+        var m = content.match(/\/\*![\s\S]*?\*\//img);
+        return m && m.join('\n') + '\n' || '';
+    }
+  })).pipe(gulp.dest('example/dest'));
+});
+ 
+
+
+//Watch task
+gulp.task('watch', ['browserSync', 'styles', 'prefix'], function() {
+    gulp.watch('sass/**/*.sass', ['prefix']);
+    gulp.watch('sass/**/*.sass',['styles']);
+	gulp.watch('sass/**/*.sass', browserSync.reload);
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('*.js', browserSync.reload);
+});
+
+// Babel
+gulp.task("babel", function () {
+  return gulp.src("./script.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+});
